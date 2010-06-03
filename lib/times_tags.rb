c6b760711c383ab34701d06a095e7af008fc869b
Jacqui Maher <jacqui@brighter.net>
2010-06-03 15:21:50 -0400

    TimesTags

---
+++ lib/times_tags.rb
@@ -0,0 +1,62 @@
+require 'httparty'
+
+class TimesTags
+  include HTTParty
+
+  base_uri "http://api.nytimes.com/svc/timestags"
+  format :json
+
+  def self.api_key
+    config['api_key']
+  end
+
+  def self.config
+    @config ||= YAML::load_file(config_path)
+  end
+
+  # Path to the times_topics.yml file.
+  def self.config_path
+    if @config_path.nil?
+      if defined?(Rails.env)
+        @config_path = File.join(RAILS_ROOT, 'config', 'times_topics.yml')
+      end
+    end
+    @config_path
+  end
+
+  def self.config_path= value
+    @config_path = value
+  end
+
+  def self.person_search(name)
+    get('/suggest', :query => { :query => name, :filter => '(Per)'})
+  end
+
+  def self.organization_search(name)
+    get('/suggest', :query => { :query => name, :filter => '(Org)'})
+  end
+
+  def self.geographic_search(name)
+    get('/suggest', :query => { :query => name, :filter => '(Geo)'})
+  end
+
+  def self.subject_search(name)
+    get('/suggest', :query => { :query => name, :filter => '(Des)'})
+  end
+
+  def self.search(query, type)
+    case type
+    when /person/
+      person_search(query)
+    when /organization/
+      organization_search(query)
+    when /geographic/
+      geographic_search(query)
+    when /subject|description/
+      subject_search(query)
+    else
+      person_search(query)
+    end
+  end
+
+end
---
+++ test/test_times_tags.rb
@@ -0,0 +1,48 @@
+require 'helper'
+require File.join(File.dirname(__FILE__), '../lib/times_tags.rb')
+
+class TestTimesTags < Test::Unit::TestCase
+  context "API Key" do
+    should "find the api key specified in a config file" do
+      TimesTags.config_path = File.join(File.dirname(__FILE__), 'times_topics.yml')
+      assert_equal "000111222333444555666777888999abcdefg+", TimesTags.api_key
+    end
+  end
+
+  context "Searching" do
+    setup do
+      TimesTags.config_path = File.join(File.dirname(__FILE__), 'times_topics.yml')
+      TimesTags.stubs(:get)
+    end
+    should "person_search" do
+      TimesTags.expects(:get).with('/suggest', :query => { :query => 'Sebastien', :filter => '(Per)' })
+      TimesTags.person_search('Sebastien')
+    end
+    should "organization_search" do
+      TimesTags.expects(:get).with('/suggest', :query => { :query => 'Sebastien', :filter => '(Org)' })
+      TimesTags.organization_search('Sebastien')
+    end
+    should "geographic_search" do
+      TimesTags.expects(:get).with('/suggest', :query => { :query => 'Sebastien', :filter => '(Geo)' })
+      TimesTags.geographic_search('Sebastien')
+    end
+    should "subject_search" do
+      TimesTags.expects(:get).with('/suggest', :query => { :query => 'Sebastien', :filter => '(Des)' })
+      TimesTags.subject_search('Sebastien')
+    end
+    should "search" do
+      TimesTags.expects(:get).with('/suggest', :query => { :query => 'Sebastien', :filter => '(Des)' })
+      TimesTags.search('Sebastien', 'subject')
+
+      TimesTags.expects(:get).with('/suggest', :query => { :query => 'Sebastien', :filter => '(Geo)' })
+      TimesTags.search('Sebastien', 'geographic')
+
+      TimesTags.expects(:get).with('/suggest', :query => { :query => 'Sebastien', :filter => '(Org)' })
+      TimesTags.search('Sebastien', 'organization')
+
+      TimesTags.expects(:get).with('/suggest', :query => { :query => 'Sebastien', :filter => '(Per)' })
+      TimesTags.search('Sebastien', 'person')
+    end
+
+  end
+end
